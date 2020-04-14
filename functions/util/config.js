f3c379e18f985e3de766db2ba1e96a719a09b4f4
Anand101094 <49559852+Anand101094@users.noreply.github.com>
2020-04-14 17:57:34 +0530

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyDeY7Xe46OnxjL2BiRcvkLnFckacSe2yTw",
-    authDomain: "anarat-91221.firebaseapp.com",
-    databaseURL: "https://anarat-91221.firebaseio.com",
-    projectId: "anarat-91221",
-    storageBucket: "anarat-91221.appspot.com",
-    messagingSenderId: "44613766413",
-    appId: "1:44613766413:web:4fa00b44cb740d99e43444",
-    measurementId: "G-P700FZ8EGX"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxxxxxx.firebaseio.com",
+    projectId: "xxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "x:xxxxxxxxxx:xxx:xxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxx"
   };
